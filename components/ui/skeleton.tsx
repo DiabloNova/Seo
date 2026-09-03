052900c8237ed760d371fc3641af1c59199658b4
DiabloNova <121438027+DiabloNova@users.noreply.github.com>
2026-09-03 12:59:53 +0330

    Delete components/ui directory

--- components/ui/skeleton.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-import { cn } from '../../lib/utils'
-
-function Skeleton({ className, ...props }: React.ComponentProps<'div'>) {
-  return <div role="status" aria-label="Loading" data-slot="skeleton" className={cn('skeleton-shimmer rounded-md', className)} {...props} />
-}
-
-export { Skeleton }
